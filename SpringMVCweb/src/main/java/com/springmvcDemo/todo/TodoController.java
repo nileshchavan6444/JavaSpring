6e5d2b5e3d19732ccf8a48c9243efb9404d78312
nileshchavan6444 <nileshchavan6444@gmail.com>
2022-04-20 21:15:27 +0530

    todo

--- SpringMVCweb/src/main/java/com/springmvcDemo/todo/TodoController.java
+++ SpringMVCweb/src/main/java/com/springmvcDemo/todo/TodoController.java
@@ -1,11 +1,14 @@
 package com.springmvcDemo.todo;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class TodoController {
@@ -21,4 +24,24 @@ public class TodoController {
 		return "list-todos";
 		
 	}
+	
+	@GetMapping("/todo/{user}")
+	public String getTodosBasedOnUser(@PathVariable String user,ModelMap model)
+	{
+		List<Todo> listOfTodos = service.retrieveTodosBasedOnUsername(user);
+		model.addAttribute("todos",listOfTodos);
+		return "list-todos";
+	}
+	
+	@GetMapping("/addtodo")
+	public String addTodos(ModelMap model)
+	{
+		service.addTodos("Sunanda", "Learn Spring MVC", new Date(), false);
+	
+		List<Todo> listOfTodos = service.retrieveTodos();
+		model.addAttribute("todos",listOfTodos);	
+		return "list-todos";
+	}
+	
+	
 }
--- SpringMVCweb/src/main/java/com/springmvcDemo/todo/TodoService.java
+++ SpringMVCweb/src/main/java/com/springmvcDemo/todo/TodoService.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
-
 @Service
 public class TodoService {
 
@@ -16,11 +15,30 @@ public class TodoService {
 	static {
 		todos.add(new Todo(1, "nilesh", "Learn spring mvc", new Date(), false));
 		todos.add(new Todo(2, "nilesh", "learn core java", new Date(), false));
-		todos.add(new Todo(3, "nilesh", "learn Hibernate", new Date(), false));
+		todos.add(new Todo(3, "John", "learn Hibernate", new Date(), false));
 
 	}
 
+	
 	public List<Todo> retrieveTodos() {
 		return todos;
+	}
+
+	
+	public List<Todo> retrieveTodosBasedOnUsername(String name) {
+		
+		List<Todo> filteredTodo = new ArrayList<Todo>();
+
+		for (Todo todo : todos) {
+			if (todo.getUser().equalsIgnoreCase(name))
+				filteredTodo.add(todo);
+		}
+		return filteredTodo;
+
+	}
+	
+	public void addTodos(String user,String desc,Date targetDate,boolean isDone)
+	{
+		todos.add(new Todo(++todoCount, user, desc, targetDate, isDone));
 	}
 }
